ff5637edfcbe29c3aba424d03ad50c07d312f87b
Mohammad Alaei <alaei.py@gmail.com>
2022-03-12 14:29:37 +0330

    Complete section 11-Arrays-Bankist.

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -78,8 +78,8 @@ const createUsernames = function (accounts) {
 createUsernames(accounts);
 /////////////////////////////////////////////////////
 
-const displayMovements = function (account) {
-  const movements = account.movements;
+const displayMovements = function (movements, sort) {
+  movements = sort ? movements.sort((a, b) => a - b) : movements;
   containerMovements.innerHTML = "";
   movements.forEach(function (movement, index) {
     const type = movement > 0 ? "deposit" : "withdrawal";
@@ -122,7 +122,7 @@ const calcDisplayBalance = function (account) {
 ////////////////////////////////////////////////////
 const updateUI = function (accout) {
   //Display movements
-  displayMovements(accout);
+  displayMovements(accout.movements);
   //Display balance
   calcDisplayBalance(accout);
   //Display Summary
@@ -190,6 +190,13 @@ btnLoan.addEventListener("click", function (e) {
     updateUI(currentAccout);
   }
   inputLoanAmount.value = "";
+});
+/////////////////////////////////////////////////
+let sorted = true;
+btnSort.addEventListener("click", function (e) {
+  e.preventDefault();
+  displayMovements([...currentAccout.movements], sorted);
+  sorted = !sorted;
 });
 /////////////////////////////////////////////////
 // LECTURES
@@ -430,3 +437,115 @@ const c = account1.movements.every((mov) => mov > 0);
 console.log(c);
 */
 ///////////////////////////////////////////////////////////
+// More Ways of Creating and Filling Arrays
+/*
+console.log([1, 2, 3, 4, 5, 6, 7]);
+console.log(new Array([1, 2, 3, 4, 5, 6, 7]));
+
+const x = new Array(7);
+console.log(x);
+
+x.fill(23, 2, 5);
+console.log(x);
+x.fill(2);
+console.log(x);
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+*/
+///////////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+GOOD LUCK 😀
+*/
+/*
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
+
+//1)
+
+dogs.forEach((dog) => (dog.recommendedFood = dog.weight ** 0.75 * 28));
+console.log(dogs);
+
+// 2)
+const sarahsDog = dogs.find((dog) => dog.owners.includes("Sarah"));
+console.log(
+  `It's eating too ${
+    sarahsDog.curFood > sarahsDog.recommendedFood ? "much" : "little"
+  } than recommened food!`
+);
+
+//3)
+const ownersEatTooMuch = dogs
+  .filter((dog) => dog.curFood > dog.recommendedFood)
+  .flatMap((dog) => dog.owners);
+
+const ownersEatTooLittle = dogs
+  .filter((dog) => dog.curFood < dog.recommendedFood)
+  .flatMap((dog) => dog.owners);
+
+//4)
+
+console.log(`${ownersEatTooMuch.join(" and ")}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(" and ")}'s dogs eat too little!`);
+
+// 5)
+console.log(dogs.some((dog) => dog.curFood === dog.recommendedFood));
+
+//6)
+console.log(
+  dogs.some(
+    (dog) =>
+      dog.curFood <= dog.recommendedFood * 1.1 &&
+      dog.curFood >= dog.recommendedFood * 0.9
+  )
+);
+
+//7)
+
+const okayFoodDogs = dogs.filter(
+  (dog) =>
+    dog.curFood <= dog.recommendedFood * 1.1 &&
+    dog.curFood >= dog.recommendedFood * 0.9
+);
+
+console.log(okayFoodDogs);
+
+//8)
+console.log(
+  dogs.slice().sort((dog1, dog2) => dog1.recommendedFood - dog2.recommendedFood)
+);
+console.log(dogs);
+*/
