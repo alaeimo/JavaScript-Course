8cb3ec8390ddeb4542de6b1edeb7b90ae1df8144
Mohammad Alaei <alaei.py@gmail.com>
2022-03-17 23:00:15 +0330

    Complete 11/16 of Mapty app (Managing Workout Data Creating Classes).

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -11,6 +11,47 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + " ").slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat,lng]
+    this.distance = distance; //Km
+    this.duration = duration; //min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    //cadence = duration(min)/distance (km)
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    //speed = distance(km) / duration (h)
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([25, -12], 2.5, 25, 16);
+const cycling1 = new Cycling([25, -12], 25, 95, 18);
+
+console.log(run1);
+console.log(cycling1);
 class App {
   #map;
   #mapEvent;
