7249526171e22708f97548d3a9dc4848e268c76b
Mohammad Alaei <alaei.py@gmail.com>
2022-03-17 19:12:37 +0330

    Complete 07/16 of Mapty app (Displaying a Map Marker).

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -24,7 +24,22 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords).addTo(map).bindPopup("My current location").openPopup();
+      map.on("click", function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 150,
+              minWidth: 75,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
     },
     () => console.log(`Couldn't access to your current location!`)
   );
