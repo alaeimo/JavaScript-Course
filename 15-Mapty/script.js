ba9b42765db3a01db94bf63a2f745407ecff8f1a
Mohammad Alaei <alaei.py@gmail.com>
2022-03-18 20:14:12 +0330

    Complete section 15-Mapty app.

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
 const inputType = document.querySelector(".form__input--type");
@@ -20,6 +17,9 @@ class Workout {
   }
 
   _workoutDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
     return `${this.type[0].toUpperCase() + this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
