f5d9d35998ca3cc0a4f85e5a4f0f3531493b44a8
Mohammad Alaei <alaei.py@gmail.com>
2022-04-04 09:29:16 +0430

    Complete 19/24 of OOP (Another Class Example).

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -469,7 +469,7 @@ sarah.calcAge();
 
 /////////////////////////////////////////////////////////////
 //Inheritance Between Classes Object.create
-
+/*
 const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
@@ -496,3 +496,49 @@ const jay = Object.create(StudentProto);
 jay.init("Jay Martin", 1996, "Computer Science");
 jay.introduce();
 jay.calcAge();
+*/
+/////////////////////////////////////////////////////////////
+//Another Class Example
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this._pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log("Loan approved");
+    }
+  }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+console.log(acc1);
+
+// acc1.movements.push(300);
+// acc1.movements.push(-150);
+acc1.deposit(300);
+acc1.withdraw(150);
+acc1.requestLoan(1000);
+acc1.approveLoan(3000);
+console.log(acc1);
+
+///////////////////////////////////////////////////////
