12c751a62c4c17d6bfde0086769fab1063fca6f0
Mohammad Alaei <alaei.py@gmail.com>
2022-04-04 09:39:44 +0430

    Complete 20/24 of OOP (Encapsulation Protected Properties and Methods).

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -504,26 +504,31 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin;
-    this.movements = [];
     this.locale = navigator.language;
+    //Protected properties
+    this._pin = pin;
+    this._movements = [];
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log("Loan approved");
     }
@@ -533,12 +538,12 @@ class Account {
 const acc1 = new Account("Jonas", "EUR", 1111);
 console.log(acc1);
 
-// acc1.movements.push(300);
-// acc1.movements.push(-150);
+// acc1._movements.push(300);
+// acc1._movements.push(-150);
 acc1.deposit(300);
 acc1.withdraw(150);
 acc1.requestLoan(1000);
-acc1.approveLoan(3000);
+acc1._approveLoan(3000);
 console.log(acc1);
 
 ///////////////////////////////////////////////////////
